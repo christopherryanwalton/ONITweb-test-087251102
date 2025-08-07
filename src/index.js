eca5b19f5d57cae519ff00d0ee8389fabf17400a
Christopher Walton <46746314+christopherryanwalton@users.noreply.github.com>
2025-08-07 01:03:25 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import App from './App';
+import App from './app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,3 +12,4 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
+
