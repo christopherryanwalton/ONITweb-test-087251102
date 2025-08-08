6230a1939af5b14a150c75fabf9519fe62a9f551
Christopher Walton <46746314+christopherryanwalton@users.noreply.github.com>
2025-08-07 22:22:20 -0400

    Create ServerRack.js

---
+++ src/components/ServerRack.js
@@ -0,0 +1,10 @@
+
+import React from 'react';
+
+const ServerRack = () => {
+  return (
+    <div className="fixed bottom-5 right-5 w-[120px] h-[200px] opacity-[0.05] z-[-1]">
+      {/* SVG server rack with animated LED lights */}
+    </div>
+  );
+};
