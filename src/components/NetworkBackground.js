f8652857ad20e467136ce7a000c08311ed6d8145
Christopher Walton <46746314+christopherryanwalton@users.noreply.github.com>
2025-08-07 22:21:55 -0400

    Create NetworkBackground.js

---
+++ src/components/NetworkBackground.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NetworkBackground = () => {
+  return (
+    <svg className="fixed top-0 left-0 w-full h-full z-[-1] opacity-[0.03] pointer-events-none">
+      {/* Grid pattern, animated lines, and pulsing nodes */}
+    </svg>
+  );
+};
